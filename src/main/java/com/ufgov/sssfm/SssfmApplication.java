1e6bc43793920ebf2a9cede76bc11d3284425f28
a4goku <wangyi_062500@hotmail.com>
2018-09-28 11:39:19 +0800

    Initial commit

---
+++ src/main/java/com/ufgov/sssfm/SssfmApplication.java
@@ -0,0 +1,12 @@
+package com.ufgov.sssfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssfmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SssfmApplication.class, args);
+    }
+}
