41a4c92c6d440488df214b737c98a3f879b591da
a4goku <wangyi_062500@hotmail.com>
2018-09-28 11:44:05 +0800

    复制代码

--- src/main/java/com/ufgov/sssfm/SssfmApplication.java
+++ src/main/java/com/ufgov/sssfm/SssfmApplication.java
@@ -2,11 +2,22 @@ package com.ufgov.sssfm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SssfmApplication {
 
     public static void main(String[] args) {
+        //SpringApplication.run(SssfmApplication.class, args);
         SpringApplication.run(SssfmApplication.class, args);
+        //新建线程类
+        //SocketThread socketThread = new SocketThread(null);
+        //启动线程
+        //socketThread.start();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SssfmApplication.class);
     }
 }
